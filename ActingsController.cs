dc2ecef1b717b0c28075c3f00962b31ef25bfc52
Ashok Vardhan Reddy <37582897+yellaashok@users.noreply.github.com>
2019-08-01 22:06:34 +0530

    Add files via upload

---
+++ ActingsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class ActingsController : Controller
+    {
+        private DBModel db = new DBModel();
+
+        
+        public ActionResult Index()
+        {
+            return View(db.Actings.ToList());
+        }
+
+        // GET: Actings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acting acting = db.Actings.Find(id);
+            if (acting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acting);
+        }
+
+        // GET: Actings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,DOB,Bio")] Acting acting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Actings.Add(acting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(acting);
+        }
+
+        // GET: Actings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acting acting = db.Actings.Find(id);
+            if (acting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acting);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,DOB,Bio")] Acting acting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(acting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(acting);
+        }
+
+        // GET: Actings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acting acting = db.Actings.Find(id);
+            if (acting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acting);
+        }
+
+        // POST: Actings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Acting acting = db.Actings.Find(id);
+            db.Actings.Remove(acting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
---
+++ MoviesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+
+        private DBModel db = new DBModel();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Act).Include(m => m.Actor);
+            return View(movies.ToList());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movy movy = db.Movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movy);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            ViewBag.MovieId = new SelectList(db.Acts, "ActorId", "Name");
+            ViewBag.MovieId = new SelectList(db.Actors, "ActorId", "Name");
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MovieId,Name,ReleaseYear,Plot,Image")] Movy movy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+          
+            return View(movy);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movy movy = db.Movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+         
+            return View(movy);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MovieId,Name,ReleaseYear,Plot,Image")] Movy movy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            return View(movy);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movy movy = db.Movies.Find(id);
+            if (movy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movy);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movy movy = db.Movies.Find(id);
+            db.Movies.Remove(movy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+      
+    }
+}
